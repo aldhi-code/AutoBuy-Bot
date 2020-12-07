162f4a5f818b58d254ae2414084d4869ba4a0e70
Tobias Polley <tobia.polle@ohs-bensberg.de>
2020-12-07 19:53:00 +0100

    ini of main and getURL

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AutoBuy-Bot
 Buyes automatically exclusive things from shops like Supreme, Nike or else
+
+# Use
+
+Just run the main.cs! Have fun!
---
+++ src/main/main.cs
@@ -0,0 +1,12 @@
+public class Main {
+
+    private String url;
+
+    public static void main(String args[]) {
+        
+    }
+
+    public void getURL(String url) {
+        return url;
+    }
+}
